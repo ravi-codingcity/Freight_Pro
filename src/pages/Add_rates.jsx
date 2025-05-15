2831b9cdaf36cf11e9ecdf507d8ef5e49375b258
ravicodingcity <ravich27@outlook.com>
2025-05-15 12:39:15 +0530

    Transit Time

--- src/pages/Add_rates.jsx
+++ src/pages/Add_rates.jsx
@@ -1894,19 +1894,18 @@ const Add_rates = () => {
                             ))}
                           </select>
                           <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                           <svg
-  xmlns="http://www.w3.org/2000/svg"
-  className="h-5 w-5 text-gray-400"
-  viewBox="0 0 20 20"
-  fill="currentColor"
->
-  <path
-    fillRule="evenodd"
-    d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 011.08 1.04l-4.25 4.65a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z"
-    clipRule="evenodd"
-  />
-</svg>
-
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-5 w-5 text-gray-400"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 011.08 1.04l-4.25 4.65a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
                           </div>
                         </div>
                       </div>
