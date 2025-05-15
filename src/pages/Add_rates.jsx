6851d09e372c87f732c247606dc69650c34028b7
ravicodingcity <ravich27@outlook.com>
2025-05-15 14:58:56 +0530

    Delete btn changes

--- src/pages/Add_rates.jsx
+++ src/pages/Add_rates.jsx
@@ -2293,8 +2293,8 @@ const Add_rates = () => {
                         <svg
                           className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
                           xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
                           viewBox="0 0 24 24"
+                          fill="none"
                         >
                           <circle
                             className="opacity-25"
@@ -2303,13 +2303,14 @@ const Add_rates = () => {
                             r="10"
                             stroke="currentColor"
                             strokeWidth="4"
-                          ></circle>
+                          />
                           <path
                             className="opacity-75"
                             fill="currentColor"
                             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                          ></path>
+                          />
                         </svg>
+
                         {editFormId ? "Updating Rate..." : "Submitting Rate..."}
                       </div>
                     ) : (
@@ -2336,15 +2337,15 @@ const Add_rates = () => {
                           <>
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
-                              className="h-5 w-5 mr-2"
+                              className="h-5 w-5 mr-2 text-white"
                               fill="none"
                               viewBox="0 0 24 24"
                               stroke="currentColor"
+                              strokeWidth={2}
                             >
                               <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
-                                strokeWidth={2}
                                 d="M9 12l2 2 4-4m6-2a9 9 0 11-18 0 9 9 0 0118 0z"
                               />
                             </svg>
@@ -2762,11 +2763,12 @@ const Add_rates = () => {
                                   fill="none"
                                   viewBox="0 0 24 24"
                                   stroke="currentColor"
+                                  strokeWidth={2}
                                 >
                                   <path
-                                    fillRule="evenodd"
-                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.292l3.71-4.06a.75.75 0 011.08 1.04l-4.25 4.65a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z"
-                                    clipRule="evenodd"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
                                   />
                                 </svg>
                               </button>
